4700d947509a44c1f685999463f3a49be02c8a66
Aravinthsamy S <63273937+aravinthsamys@users.noreply.github.com>
2026-01-14 16:34:23 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -173,8 +173,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'meganand001@gmail.com'
-EMAIL_HOST_PASSWORD = 'birbioapifjtwkxr'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
