239e9d90c97e422947d1752e613afbaed0e32a57
Aravinthsamy S <63273937+aravinthsamys@users.noreply.github.com>
2026-01-14 16:50:54 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -169,13 +169,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND","django.core.mail.backends.console.EmailBackend")
-EMAIL_HOST = Env("EMAIL_HOST")
-EMAIL_PORT = int(Env("EMAIL_PORT", 587))
-EMAIL_USE_TLS =  Env("EMAIL_USE_TLS") == "True"
-
-EMAIL_HOST_USER = Env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = Env("EMAIL_HOST_PASSWORD")
-
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
